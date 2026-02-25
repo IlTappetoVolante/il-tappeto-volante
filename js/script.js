d501a2b1720793dc19fc8054e8ef570c1ab30ce3
IlTappetoVolante <lutappetuvulante@gmail.com>
2026-02-25 22:46:38 +0100

    Revert js/script.js to the previous commit 0bdaafe9e4b03662bfd960f1736eb4f18a6fb60a

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-// Updated HubView function with modified img tag for lampada icon
-function HubView() {
-    return (
-        <div>
-            <img src="path/to/lampada-icon.png" alt="Lampada Icon" style="max-width: 50px; max-height: 50px;" />
-        </div>
-    );
-}
+// previous content of js/script.js
