5272ce6c6f37dd43549d6c7f804b30b210a74844
IlTappetoVolante <lutappetuvulante@gmail.com>
2026-03-15 00:47:03 +0100

    update js/contenuti/tappa1.js

--- js/contenuti/tappa1.js
+++ js/contenuti/tappa1.js
@@ -11,7 +11,7 @@ const TAPPA_1_DATA = {
     contenuto: [
 
         { type: "testo", paragrafo: "Se c'è un luogo che definisce Istanbul così come io la vedo, non è un singolo palazzo o una moschea, ma un punto di incontro, un connettore pulsante che incarna alla perfezione quell'essenza \"affascinante e caotica\" che ho sempre percepito." },
-        { type: "immagine", src: "https://lh3.googleusercontent.com/d/1_CMqrx9T01xVVxAlGlLps6arwNOn1DAo" },
+        { type: "immagine", src: "img/ponte_galata_notte" },
         { type: "didascalia", testo: "L'acqua divide, il ponte abbraccia: l'anima notturna del Corno d'Oro" },
 
         { type: "testo", paragrafo: "È il Galata Köprüsü (Ponte di Galata), visto di notte. Il mio \"sguardo\" cerca di catturare proprio questo. È un'immagine che vive di contrasti: un fiume d'acqua scura e inquieta, il Corno d'Oro, tagliato da un fiume orizzontale di luce. È qui che l' \"Anima Multipla\" di Istanbul si rivela." },
