3115b0274c19b8d2d49a6c23bdbbc401d88ab85d
IlTappetoVolante <lutappetuvulante@gmail.com>
2026-03-15 17:05:02 +0100

    update js/contenuti/tappa1.js

--- js/contenuti/tappa1.js
+++ js/contenuti/tappa1.js
@@ -19,8 +19,6 @@ const TAPPA_1_DATA = {
         { type: "testo", paragrafo: "La nostra passeggiata inizia dalle Mura Terrestri (Mura Teodosiane), quelle che vediamo sul lato ovest della mappa. Queste sono le mura leggendarie che il Sultano Mehmet II Fatih (il Conquistatore) sfondò nel 1453. La storia racconta che ci riuscì grazie a cannoni giganti, i più grandi mai visti all’epoca, progettati da un ingegnere ungherese di nome Orban." },
         { type: "immagine", src: "img/mura_teodosiane_istanbul.jpg" },
         { type: "immagine", src: "img/cannone_turco_bronzo_1464.jpg" },
-        
-        
         { type: "testo", paragrafo: "La leggenda vuole che Orban avesse offerto i suoi servigi prima ai Bizantini, che però non potevano permettersi le sue tariffe. Il Sultano Fatih, invece, lo coprì d’oro. Furono quelle <em>super‑bombarde</em> a sbriciolare il settore delle mura attorno alla <strong>Porta di San Romano</strong>, oggi chiamata <strong>Topkapı Kapısı</strong> è qui che si aprì la breccia decisiva, ed è da qui che Mehmet II fece il suo ingresso nella città conquistata." },
         { type: "immagine", src: "img/porta_san_romano_istanbul.jpg" },
         { type: "testo", paragrafo: "Poco più a sud, lo sguardo incontra un’altra storia. <strong>Altınkapı</strong>, la Porta Aurea bizantina, era l’antico ingresso trionfale dell’Impero Romano d’Oriente. Non fu coinvolta nell’assalto del 1453, ma subito dopo la conquista Fatih decise di <strong>sigillarla</strong> e di costruirci attorno una nuova fortezza ottomana <strong>Yedikule</strong>, la “Fortezza delle Sette Torri”. È un’architettura ibrida, un luogo in cui le torri ottomane abbracciano e inglobano i magnifici archi di marmo della porta imperiale bizantina." },
